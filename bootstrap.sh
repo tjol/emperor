9582cf4209c0c76e4c8acc96d718c3442adffe7d
Thomas Jollans <thomas@jollans.com>
2011-07-24 14:51:01 +0200

    Use automake's default INSTALL file.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,11 +5,11 @@ mkdir -p config &&
 echo +++ Running aclocal ... &&
 aclocal &&
 echo +++ Running libtoolize ... &&
-libtoolize &&
+libtoolize --copy &&
 echo +++ Running autoconf ... &&
 autoconf && 
 echo +++ Running automake --add-missing ... &&
-automake --add-missing &&
+automake --add-missing --copy --gnu &&
 echo +++ Running automake ... &&
 automake --foreign Makefile src/Makefile &&
 echo You may now run ./configure ||
